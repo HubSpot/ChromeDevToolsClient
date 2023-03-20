e93241dc8b619cc3044a3b27343ff02054e0e3c8
William Hou <whou@hubspot.com>
2023-03-19 23:05:21 -0400

    move jsonignoreproperties to sessioninfo model

--- ChromeDevToolsBase/src/main/java/com/hubspot/chrome/devtools/base/ChromeSessionInfoIF.java
+++ ChromeDevToolsBase/src/main/java/com/hubspot/chrome/devtools/base/ChromeSessionInfoIF.java
@@ -1,9 +1,11 @@
 package com.hubspot.chrome.devtools.base;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.immutables.value.Value.Immutable;
 
 @Immutable
 @ChromeStyle
+@JsonIgnoreProperties(ignoreUnknown = true) // Ignore unknown properties to handle chrome adding fields to base models
 public interface ChromeSessionInfoIF {
   String getDescription();
   String getDevtoolsFrontendUrl();
--- ChromeDevToolsBase/src/main/java/com/hubspot/chrome/devtools/base/ChromeStyle.java
+++ ChromeDevToolsBase/src/main/java/com/hubspot/chrome/devtools/base/ChromeStyle.java
@@ -1,19 +1,17 @@
 package com.hubspot.chrome.devtools.base;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import org.immutables.value.Value;
+import org.immutables.value.Value.Style.ImplementationVisibility;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import org.immutables.value.Value;
-import org.immutables.value.Value.Style.ImplementationVisibility;
-
 @Target({ ElementType.PACKAGE, ElementType.TYPE })
 @Retention(RetentionPolicy.CLASS) // Make it class retention for incremental compilation
-@JsonIgnoreProperties(ignoreUnknown = true) // Ignore unknown properties to handle chrome adding fields to base models
 @JsonSerialize
 @Value.Style(
   get = { "is*", "get*" }, // Detect 'get' and 'is' prefixes in accessor methods
@@ -22,7 +20,7 @@ import org.immutables.value.Value.Style.ImplementationVisibility;
   typeImmutable = "*", // No prefix or suffix for generated immutable type
   optionalAcceptNullable = true, // allow for an Optional<T> to have a setter that takes a null value of T
   visibility = ImplementationVisibility.SAME, // Generated class will have the same visibility as the abstract class/interface)
-  jdkOnly = true
-) // For Guava 18+, this stops MoreObjects from being used in toString and ImmutableHashMap.Builder from being used for building map fields (among other effects).
+  jdkOnly = true // For Guava 18+, this stops MoreObjects from being used in toString and ImmutableHashMap.Builder from being used for building map fields (among other effects).
+)
 public @interface ChromeStyle {
 }
